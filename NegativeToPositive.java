d8a31786b54176fa03bbc5cb92bda305c68b13ec
Elamathi-Manickavasakan <102462400+Elamathi-Manickavasakan@users.noreply.github.com>
2022-05-10 14:45:03 +0530

    Add files via upload

---
+++ NegativeToPositive.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class NegativeToPositive {
+
+public static void main(String[] args) {
+	
+	int x = -40;
+	if (x>0) {
+		System.out.println("The number -40 is converted to 40");
+	}
+	else if (x<0) {
+		System.out.println("The number -40 is converted to 40");
+	}
+	else {
+		System.out.println("The number -40 is converted to 40");
+	}
+	
+	}
+
+    }
---
+++ PositiveorNegative.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class PositiveorNegative {
+	
+public static void main(String[] args) {
+	int x = 67;
+	if(x>0) {
+		System.out.println("The number is positive");
+	}
+	else if (x<0) {
+		System.out.println("The number is negative");
+	}
+	else {
+		System.out.println("The number is neither positive nor negative");
+	}
+  
+    }
+
+    }
